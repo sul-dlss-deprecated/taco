eabd653d2077b3a7e80a2793adaae03e4f625c2e
Justin Coyne <jcoyne@justincoyne.com>
2018-01-25 14:51:14 -0800

    Move tests to the handlers package

---
+++ handlers/server.go
@@ -0,0 +1,27 @@
+package handlers
+
+import (
+	"log"
+
+	"github.com/go-openapi/loads"
+	"github.com/sul-dlss-labs/taco"
+	"github.com/sul-dlss-labs/taco/generated/restapi"
+	"github.com/sul-dlss-labs/taco/generated/restapi/operations"
+)
+
+// create new service API
+func BuildAPI(rt *taco.Runtime) *operations.TacoAPI {
+	api := operations.NewTacoAPI(swaggerSpec())
+	api.RetrieveResourceHandler = NewRetrieveResource(rt)
+	api.DepositNewResourceHandler = NewDepositResource(rt)
+	return api
+}
+
+func swaggerSpec() *loads.Document {
+	// load embedded swagger file
+	swaggerSpec, err := loads.Analyzed(restapi.SwaggerJSON, "")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return swaggerSpec
+}
