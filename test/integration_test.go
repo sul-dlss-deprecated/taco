58363d5efa044716fdb93d83bc43fcfa497c2ac5
Justin Coyne <jcoyne@justincoyne.com>
2018-01-26 15:48:15 -0600

    Install localstack on circle ci and do an integration test

---
+++ test/integration_test.go
@@ -0,0 +1,46 @@
+package test
+
+import (
+	"flag"
+	"fmt"
+	"testing"
+
+	baloo "gopkg.in/h2non/baloo.v3"
+)
+
+var port string
+
+// For a custom port, invoke this as:
+//   go test test/integration_test.go --port 3000
+func init() {
+	flag.StringVar(&port, "port", "8080", "port for test server")
+}
+
+func setupTest() *baloo.Client {
+	return baloo.New(fmt.Sprintf("http://localhost:%s", port))
+}
+
+const schema = `{
+  "title": "Create response",
+  "type": "object",
+  "properties": {
+    "id": {
+      "type": "string"
+    }
+  },
+  "required": ["id"]
+}`
+
+func TestBalooSimple(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test in short mode")
+	}
+	setupTest().Post("/v1/resource").
+		SetHeader("Foo", "Bar").
+		JSON(map[string]string{"title": "value1", "sourceId": "value2"}).
+		Expect(t).
+		Status(200).
+		Type("json").
+		JSONSchema(schema).
+		Done()
+}
