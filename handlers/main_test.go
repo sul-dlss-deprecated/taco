a0779f4d34e7518f49e6ae1199bcf9d23499a7f8
Justin Coyne <jcoyne@justincoyne.com>
2018-01-29 16:35:50 -0600

    Persist a record in the repository
    Ref #99

--- handlers/main_test.go
+++ handlers/main_test.go
@@ -10,24 +10,29 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/sul-dlss-labs/taco"
 	"github.com/sul-dlss-labs/taco/config"
-	"github.com/sul-dlss-labs/taco/generated/models"
 	"github.com/sul-dlss-labs/taco/persistence"
 )
 
-func mockRepo(record *models.Resource) persistence.Repository {
-	return &fakeRepository{record: record}
+func mockRepo(record *persistence.Resource) persistence.Repository {
+	return &fakeRepository{record: record, CreatedResources: []persistence.Resource{}}
 }
 
 type fakeRepository struct {
-	record *models.Resource
+	record           *persistence.Resource
+	CreatedResources []persistence.Resource
 }
 
-func (f fakeRepository) GetByID(id string) (*models.Resource, error) {
+func (f *fakeRepository) GetByID(id string) (*persistence.Resource, error) {
 
 	if f.record != nil {
 		return f.record, nil
 	}
 	return nil, errors.New("not found")
+}
+
+func (f *fakeRepository) SaveItem(resource *persistence.Resource) error {
+	f.CreatedResources = append(f.CreatedResources, *resource)
+	return nil
 }
 
 func setupFakeRuntime(repo persistence.Repository) http.Handler {
@@ -39,7 +44,7 @@ func setupFakeRuntime(repo persistence.Repository) http.Handler {
 func TestRetrieveHappyPath(t *testing.T) {
 	r := gofight.New()
 	r.GET("/v1/resource/99").
-		Run(setupFakeRuntime(mockRepo(new(models.Resource))),
+		Run(setupFakeRuntime(mockRepo(new(persistence.Resource))),
 			func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
 				assert.Equal(t, http.StatusOK, r.Code)
 			})
@@ -51,32 +56,5 @@ func TestRetrieveNotFound(t *testing.T) {
 		Run(setupFakeRuntime(mockRepo(nil)),
 			func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
 				assert.Equal(t, http.StatusNotFound, r.Code)
-			})
-}
-
-func TestCreateResourceHappyPath(t *testing.T) {
-	r := gofight.New()
-	r.POST("/v1/resource").
-		SetJSON(gofight.D{
-			"id":       "oo000oo0001",
-			"sourceId": "bib12345678",
-			"title":    "My work",
-		}).
-		Run(setupFakeRuntime(mockRepo(nil)),
-			func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
-				assert.Equal(t, http.StatusOK, r.Code)
-			})
-}
-
-func TestCreateResourceMissingSourceId(t *testing.T) {
-	r := gofight.New()
-	r.POST("/v1/resource").
-		SetJSON(gofight.D{
-			"id":    "oo000oo0001",
-			"title": "My work",
-		}).
-		Run(setupFakeRuntime(mockRepo(nil)),
-			func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
-				assert.Equal(t, http.StatusUnprocessableEntity, r.Code)
 			})
 }
--- persistence/repository.go
+++ persistence/repository.go
@@ -8,33 +8,59 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/spf13/viper"
-	"github.com/sul-dlss-labs/taco/generated/models"
 )
 
 // NewRepository -- Creates a new repository
 func NewRepository(config viper.Viper, db *dynamodb.DynamoDB) (*DynamoRepository, error) {
-	return &DynamoRepository{config: config, db: db}, nil
+	tableName := aws.String(config.GetString("resource_repository.table_name"))
+	return &DynamoRepository{config: config,
+			db:        db,
+			tableName: tableName},
+		nil
 }
 
 type Repository interface {
-	GetByID(string) (*models.Resource, error)
+	GetByID(string) (*Resource, error)
+	SaveItem(*Resource) error
 }
 
 // DynamoRepository -- The fetching object
 type DynamoRepository struct {
-	config viper.Viper
-	db     *dynamodb.DynamoDB
+	config    viper.Viper
+	db        *dynamodb.DynamoDB
+	tableName *string
+}
+
+// SaveItem perist the resource in dynamo db
+func (h DynamoRepository) SaveItem(resource *Resource) error {
+	row, err := dynamodbattribute.MarshalMap(resource)
+
+	if err != nil {
+		return err
+	}
+
+	input := &dynamodb.PutItemInput{
+		Item:      row,
+		TableName: h.tableName,
+	}
+
+	_, err = h.db.PutItem(input)
+
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // GetByID -- given an identifier, find the resource
-func (h DynamoRepository) GetByID(id string) (*models.Resource, error) {
+func (h DynamoRepository) GetByID(id string) (*Resource, error) {
 	params := &dynamodb.GetItemInput{
 		Key: map[string]*dynamodb.AttributeValue{
 			"id": {
 				S: aws.String(id),
 			},
 		},
-		TableName:      aws.String("resources"),
+		TableName:      h.tableName,
 		ConsistentRead: aws.Bool(true),
 	}
 	resp, err := h.db.GetItem(params)
@@ -42,7 +68,7 @@ func (h DynamoRepository) GetByID(id string) (*models.Resource, error) {
 		log.Println(err)
 		return nil, err
 	}
-	var resource *models.Resource
+	var resource *Resource
 	if err := dynamodbattribute.UnmarshalMap(resp.Item, &resource); err != nil {
 		log.Println(err)
 		return nil, err
