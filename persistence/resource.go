256bb40a926fba5c4498057e5733bc0ac3145761
Christina Harlow <cmharlow@gmail.com>
2018-01-31 11:34:04 -0800

    updating deposit resource handler for updated spec model

--- persistence/resource.go
+++ persistence/resource.go
@@ -1,10 +1,19 @@
 package persistence
 
+import (
+	"github.com/go-openapi/strfmt"
+)
+
 // Resource represents the resource as it exists in the persistence layer
 // this is very similar to models.Resource, but COULD vary, so we should
 // keep them separated
 type Resource struct {
-	ID       string `json:"id"`
-	Title    string `json:"title"`
-	SourceID string `json:"source_id"`
+	ID        string     `json:"id"`
+	AtType    strfmt.URI `json:"@type"`
+	AtContext strfmt.URI `json:"@context"`
+	Access    string     `json:"@access"`
+	Label     string     `json:"label"`
+	Preserve  bool       `json:"@preserve"`
+	Publish   bool       `json:"@publish"`
+	SourceID  string     `json:"source_id"`
 }
