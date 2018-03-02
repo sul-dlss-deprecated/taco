9f7a5edaad668b876ca44e57949654df47af00d8
Justin Coyne <jcoyne@justincoyne.com>
2018-03-01 20:31:45 -0600

    Fix config boolean bug and reduce extraneous logging

--- config/config.go
+++ config/config.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"reflect"
 	"strconv"
-	"strings"
 )
 
 // Config is configuration for the TACO application
@@ -52,30 +51,36 @@ func setString(field reflect.Value, envVariable string, defaultValue string) {
 	if value == "" {
 		value = defaultValue
 		log.Printf("%s: Using default [%s].", envVariable, defaultValue)
+	} else {
+		log.Printf("%s: Found setting [%s].", envVariable, value)
 	}
-	log.Printf("%s: Found setting [%s].", envVariable, value)
 	field.SetString(value)
 }
 
 func setBool(field reflect.Value, envVariable string, defaultValue string) {
-	// defaultValue, _ := strconv.ParseBool(defaultValueAsString)
 	value := os.Getenv(envVariable)
-	if value == "" || strings.ToUpper(value) == "FALSE" {
-		log.Printf("%s: Using default [FALSE].", envVariable)
-		field.SetBool(false)
+	if value == "" {
+		value = defaultValue
+		log.Printf("%s: Using default [%s].", envVariable, value)
+	} else {
+		log.Printf("%s: Found setting [%s].", envVariable, value)
 	}
-	log.Printf("%s: Found setting [%s].", envVariable, value)
-	field.SetBool(true)
+	boolVal, err := strconv.ParseBool(value)
+	if err != nil {
+		log.Printf("%s: Error setting [%s] as a boolean, using [%s] instead", envVariable, value, defaultValue)
+		boolVal, _ = strconv.ParseBool(defaultValue)
+	}
+	field.SetBool(boolVal)
 }
 
 func setInteger(field reflect.Value, envVariable string, defaultValue string) {
 	value, err := strconv.ParseInt(os.Getenv(envVariable), 10, 64)
-	defaultInteger, _ := strconv.ParseInt(defaultValue, 10, 64)
-
 	if err != nil || value == 0 {
+		defaultInteger, _ := strconv.ParseInt(defaultValue, 10, 64)
 		log.Printf("%s: Using default [%v].", envVariable, defaultInteger)
 		value = defaultInteger
+	} else {
+		log.Printf("%s: Found setting [%v].", envVariable, value)
 	}
-	log.Printf("%s: Found setting [%v].", envVariable, value)
 	field.SetInt(value)
 }
