5febac279a0eb58ae1022208ea9e42dddfc1b37c
Justin Coyne <jcoyne@justincoyne.com>
2018-03-21 10:14:46 -0500

    Files must assert their containment

--- validators/maps.go
+++ validators/maps.go
@@ -1376,6 +1376,7 @@ var (
     "structural": {
       "description": "Structural metadata for the File.",
       "type": "object",
+      "required": ["isContainedBy"],
       "properties": {
         "isContainedBy": {
           "description": "Fileset that contains this File.",
