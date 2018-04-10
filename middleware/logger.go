1e9586528585dbbbff4ea9b6c3435ba3f0d31dbd
Justin Coyne <jcoyne@justincoyne.com>
2018-04-10 09:45:13 -0500

    Log errors and IP address

--- middleware/logger.go
+++ middleware/logger.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -30,25 +31,42 @@ func (rl *RequestLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
 	lrw := newLoggingResponseWriter(w)
 	rl.next.ServeHTTP(lrw, r)
-	logRequest(r, lrw.statusCode, start)
+	logRequest(r, lrw.statusCode, lrw.message, start)
 }
 
-func logRequest(r *http.Request, statusCode int, start time.Time) {
+func logRequest(r *http.Request, statusCode int, message *[]byte, start time.Time) {
 	elapsed := time.Since(start).Round(time.Microsecond * 100)
-	log.Printf("%s %s Completed %v in %s", r.Method, r.URL, statusCode, elapsed)
+	str := fmt.Sprintf("%s %s Completed %v in %s [%s]", r.Method, r.URL, statusCode, elapsed, getIPAddress(r))
+	if message != nil {
+		log.Printf("%s\n\t%s", str, string(*message))
+	} else {
+		log.Println(str)
+	}
 }
 
 type loggingResponseWriter struct {
 	http.ResponseWriter
 	statusCode int
+	message    *[]byte
 }
 
 // newLoggingResponseWriter creates a new instance of loggingResponseWriter
 func newLoggingResponseWriter(w http.ResponseWriter) *loggingResponseWriter {
-	return &loggingResponseWriter{w, http.StatusOK}
+	return &loggingResponseWriter{w, http.StatusOK, nil}
 }
 
 func (lrw *loggingResponseWriter) WriteHeader(code int) {
 	lrw.statusCode = code
 	lrw.ResponseWriter.WriteHeader(code)
+}
+
+func (lrw *loggingResponseWriter) Write(body []byte) (int, error) {
+	switch lrw.statusCode {
+	case 200, 201, 404:
+		// do nothing
+	default:
+		// log the error
+		lrw.message = &body
+	}
+	return lrw.ResponseWriter.Write(body)
 }
